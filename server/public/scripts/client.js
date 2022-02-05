60ce44dd880c1f39378b97ed4ca6943a0e96f8c5
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-05 13:18:44 -0600

    Created and tested '/calculation' url between client.js and server.js that collects input values, sends data over as object, and stores data as new object in server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,42 @@ console.log('JS');
 
 $(document).ready(onReady);
 
+
 function onReady() {
     console.log('JQ');
     
-}
+    // initiate calculation on '=' click
+    $('#equalButton').on('click', bundleCalculation);
+} // end onReady
+
+let bundleCalculation = () => {
+    console.log('in bundleCalculation');
+
+        // define values taken from input fields
+        let firstNumber = Number($('#firstNumber').val());
+        let operation = 'add';
+        let secondNumber = Number($('#secondNumber').val());
+
+    // making post request to server on /calculation url
+    $.ajax({
+        method: 'POST', // type of request
+        url: '/calculation',
+        data: {
+
+            // object sent to server
+            bundledObject: {
+                firstNumber: firstNumber,
+                operation: operation,
+                secondNumber: secondNumber,
+            },
+        },
+    }).then(function(response){
+        console.log('Success!', response);
+        
+    }).catch(function(response){
+        console.log('Failure!');
+        
+    })
+    
+} // end bundleCalculation
+
--- server/server.js
+++ server/server.js
@@ -14,9 +14,46 @@ app.use(bodyParser.urlencoded({extended: true}));
 // folder name is server/public
 app.use(express.static('server/public'));
 
+// -------------------------------------------------------------------//
+
+// initialize calculationHistory an
+let calculationHistory = [{firstNumber: 23, operation: 'add', secondNumber: 1, solution: 4}];
 
 
+// server receiving request from client on /calculation url
+app.post("/calculation", (req, res) => {
+    console.log('request', req.body);
+    
+    // define variables from client post for future calculation
+    let firstNumber = Number(req.body.bundledObject.firstNumber);
+    let operation = req.body.bundledObject.operation;
+    let secondNumber = Number(req.body.bundledObject.secondNumber); 
 
+    // define bundledCalculation object
+    let bundledCalculation = {
+        firstNumber: firstNumber,
+        operation: operation,
+        secondNumber: secondNumber,
+        solution: null,
+    }
+
+    // send bundledCalculation through function to solve calculation
+    solveCalculation(bundledCalculation);
+
+    // add bundledCalculation to calculationHistory array
+    calculationHistory.push(bundledCalculation);    
+
+    // sends bundledCalculation (with solution) back to server
+    res.send(bundledCalculation);
+
+}); // end post /calculation
+
+
+// calculate problem given from client
+let solveCalculation = () =>{
+    console.log('in solveCalculation');
+        
+} // end solveCalculation
 
 
 
@@ -42,4 +79,4 @@ app.use(express.static('server/public'));
 app.listen(port, function(){
     console.log('listening on port', port);
     
-})
+});
