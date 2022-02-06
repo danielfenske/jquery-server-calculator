c4db072dfeb259cf113d55211b1c0d848648399a
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-05 20:44:15 -0600

    Added styling to #historyContainer of page; added comments to client.js for added explanation to function operations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,11 +2,13 @@ console.log('JS');
 
 $(document).ready(onReady);
 
+// initialize operation variable
 let operation;
 
 function onReady() {
     console.log('JQ');
-
+    
+    // runs function to display initial array on DOM
     getCalculationHistory();
     
     // click listener for '=' button
@@ -23,6 +25,7 @@ function onReady() {
 
 } // end onReady
 
+// operation will change value depending on operation button click
 let operationAdd = () => {
     operation = '+';
 } // end operationAdd
@@ -39,12 +42,13 @@ let operationDivide = () => {
     operation = '÷';
 } // end operationSubtract
 
+// Purpose of function: get latest calculation history array from server
 let getCalculationHistory = () => {
     console.log('in getCalculationHistory');
     
     $.ajax({
-        method: 'GET',
-        url: '/calculationHistory',
+        method: 'GET', // request to server for calculator history
+        url: '/calculationHistory', 
         data: ''
     }).then(function(response){
         console.log('Success!', response);
@@ -58,6 +62,8 @@ let getCalculationHistory = () => {
     })
 } // end getCalculations
 
+// Purpose of function: pull input values and 
+// send as bundled object to server for calculation
 let bundleCalculation = () => {
     console.log('in bundleCalculation');
 
@@ -66,49 +72,56 @@ let bundleCalculation = () => {
         let secondNumber = Number($('#secondNumber').val());
 
 if ($('.inputField').val() === '') {
+    // will run if either input field is empty - inserted to avoid null in calculations
     alert('Please enter two numbers to receive calculation');
-} else if (operation === undefined) {
-    alert('Please select operation to receive calculation');
-    } else {
-        // making post request to server on /calculation url
-        $.ajax({
-            method: 'POST', // type of request
-            url: '/calculation',
-            data: {
-
-                // object sent to server
-                bundledObject: {
-                    firstNumber: firstNumber,
-                    operation: operation,
-                    secondNumber: secondNumber,
-                },
-            },
-        }).then(function(response){
-            // console.log('Success!', response);
-
-            getCalculationHistory();
-            
-        }).catch(function(response){
-            // console.log('Failure!');
-            
-        })
     
-}
+    } else if (operation === undefined) {
+        // will run if no operation is clicked - inserted to avoid null in calculation
+        alert('Please select operation to receive calculation');
+        
+        } else {
+            // making post request to server on /calculation url
+            $.ajax({
+                method: 'POST', // request to post new object to server
+                url: '/calculation',
+                data: {
 
+                    // object sent to server
+                    bundledObject: {
+                        firstNumber: firstNumber,
+                        operation: operation,
+                        secondNumber: secondNumber,
+                    },
+                },
+            }).then(function(response){
+                console.log('Success!', response);
+
+                // call to grab latest calculation history
+                getCalculationHistory();
+                
+            }).catch(function(response){
+                console.log('Failure!');
+            })
+        
+    }
 } // end bundleCalculation
 
+// Purpose of function: append latest calculation history to DOM
 let renderCalculationHistory = (response) => {
     console.log('in renderCalculationHistory');
     
+    // empties previous calculation history array
     $('#calculatorHistory').empty();
 
+    // loops through calculation history array and appends each object to display calc + solution
     for (let calculation of response) {
         $('#calculatorHistory').append(`
-            <li>${calculation.firstNumber} ${calculation.operation} ${calculation.secondNumber} = ${calculation.solution}</li>
+            <li class="calculationLi">${calculation.firstNumber} ${calculation.operation} ${calculation.secondNumber} = ${calculation.solution}<hr></li>
         `)
     }
 } // end renderCalculationHistory
 
+// Purpose of function: clear input fields when 'C' button is pressed
 let clearInputFields = () => {
     console.log('in clearInputFields');
 
