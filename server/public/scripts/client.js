a152fddd8dc84f1f67b66e19627c2071694a15a8
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-10 22:01:44 -0600

    Tweaked functions in client.js to effectively accept firstNumber, secondNumber, and operator to perform calculation on server and return solution

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,9 +3,9 @@ console.log('JS');
 $(document).ready(onReady);
 
 // // initialize operation variable
-let operator = '';
-let numberOne = '';
-let numberTwo = '';
+let operation = '';
+let firstNumber = '';
+let secondNumber = '';
 
 function onReady() {
     console.log('JQ');
@@ -17,35 +17,49 @@ function onReady() {
     $('#equalButton').on('click', bundleCalculation);
 
     // click listener for 'c' button
-    $('#clearButton').on('click', clearInputFields);
+    $('#clearButton').on('click', resetEquationVariables);
 
     // click listener for operation buttons
-    $('.operationButton').on('click', handleOperator);
+    $('.operationButton').on('click', handleoperation);
 
     // click listener for number buttons
-    $('.numberButton').on('click', handleNumber)
+    $('.numberButton').on('click', handleNumber);
 
 } // end onReady
 
-function handleOperator() {
-    console.log('in handleOperator', $(this).data().operator);
+function renderNumber() {
+    console.log('in renderNumber');
     
-    operator = $(this).data().operator
-} // end handleOperator
+    if (operation === '') {
+        $('.inputField').val(firstNumber);
+    } else if (operation != '') {
+        clearInputField();
+
+        $('.inputField').val(secondNumber);
+    }
+} // end renderNumber
+
+function handleoperation() {
+    console.log('in handleoperation', $(this).data().operation);
+    
+    operation = $(this).data().operation
+} // end handleoperation
 
 function handleNumber() {
     console.log('in handleNumber', String($(this).data().number));
     
     let buttonPushed = String($(this).data().number);
 
-    if (operator === '') {
-        numberOne += buttonPushed;
+    if (operation === '') {
+        firstNumber += buttonPushed;
+        renderNumber();
     } else {
-        numberTwo += buttonPushed;
+        secondNumber += buttonPushed;
+        renderNumber();
     }
     
-    console.log('numberOne', numberOne);
-    console.log('numberTwo', numberTwo);
+    console.log('numberOne', firstNumber);
+    console.log('numberTwo', secondNumber);
 
     
 } // end handleNumber
@@ -72,46 +86,48 @@ let getCalculationHistory = () => {
 
 // Purpose of function: pull input values and 
 // send as bundled object to server for calculation
-let bundleCalculation = () => {
+function bundleCalculation(){
     console.log('in bundleCalculation');
-
-if ($('.inputField').val() === '') {
-    // will run if either input field is empty - inserted to avoid null in calculations
-    alert('Please enter two numbers to receive calculation');
-    
-    } else if (operation === undefined) {
+if (firstNumber === '') {
+    // will run if no operation is clicked - inserted to avoid null in calculation
+    alert('Please enter first number to receive calculation');
+  }  else if (operation === '') {
         // will run if no operation is clicked - inserted to avoid null in calculation
         alert('Please select operation to receive calculation');
         
-        } else {
-            // making post request to server on /calculation url
-            $.ajax({
-                method: 'POST', // request to post new object to server
-                url: '/calculation',
-                data: {
+        } else if (secondNumber === '') {
+            // will run if either input field is empty - inserted to avoid null in calculations
+            alert('Please enter second number to receive calculation');
+            
+            } else {
+                // making post request to server on /calculation url
+                $.ajax({
+                    method: 'POST', // request to post new object to server
+                    url: '/calculation',
+                    data: {
 
-                    // object sent to server
-                    bundledObject: {
-                        firstNumber: firstNumber,
-                        operation: operation,
-                        secondNumber: secondNumber,
+                        // object sent to server
+                        bundledObject: {
+                            firstNumber: firstNumber,
+                            operation: operation,
+                            secondNumber: secondNumber,
+                        },
                     },
-                },
-            }).then(function(response){
-                console.log('Success!', response);
+                }).then(function(response){
+                    console.log('Success!', response);
 
-                // call to grab latest calculation history
-                getCalculationHistory();
-                
-            }).catch(function(response){
-                console.log('Failure!');
-            })
-        
-    }
+                    // call to grab latest calculation history
+                    getCalculationHistory();
+                    
+                }).catch(function(response){
+                    console.log('Failure!');
+                })
+            
+        }
 } // end bundleCalculation
 
 // Purpose of function: append latest calculation history to DOM
-let renderCalculationHistory = (response) => {
+function renderCalculationHistory (response){
     console.log('in renderCalculationHistory');
     
     // empties previous calculation history array
@@ -126,10 +142,23 @@ let renderCalculationHistory = (response) => {
 } // end renderCalculationHistory
 
 // Purpose of function: clear input fields when 'C' button is pressed
-let clearInputFields = () => {
-    console.log('in clearInputFields');
+function clearInputField () {
+    console.log('in clearInputField');
 
+    // clear input field
+    $('.inputField').val('');
+
+} // end clearInputField
+
+function resetEquationVariables () {
+    console.log('in resetEquationVariables');
+    
     // clear input fields when 'c' button is clicked
     $('.inputField').val('');
 
-} // end clearInputFields
+    // resets global equation variables
+    operation = '';
+    firstNumber = '';
+    secondNumber = '';
+
+} // end resetEquationVariables
